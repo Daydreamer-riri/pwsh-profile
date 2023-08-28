f251c288429593456e4c39d2bb81b7bad084f39e
Riri <Daydreamerriri@outlook.com>
2023-08-28 11:40:28 +0800

    fix: forward word

--- user_profile.ps1
+++ user_profile.ps1
@@ -26,7 +26,7 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function Forwardord
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
 
 # Alias
 Set-Alias vim nvim
